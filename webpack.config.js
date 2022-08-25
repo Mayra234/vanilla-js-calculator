824a8e541a5b2e964c13c8a38f527b0655d13b92
Mayra234 <alejandradiavanera@gmail.com>
2022-08-25 17:42:39 -0500

    Se despliega el portafolio

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,42 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  resolve: { extensions: [".js"] },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-        type: "asset",
-      },
+module.exports = (_, args) => {
+  const env = args.mode === 'development' ? '.env.dev' : '.env.prod';
+  const mode = args.mode || 'production';
+  return {
+    entry: './src/index.jsx',
+    mode,
+    devtool: 'source-map',
+    resolve: { extensions: ['.js', '.jsx'] },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+          type: 'asset',
+        },
+        {
+          test: /\.html$/,
+          use: ['html-loader'],
+        },
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    devServer: {
+      open: true,
+      historyApiFallback: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: './src/index.html' }),
+      new Dotenv({ path: env }),
     ],
-  },
-  devServer: {
-    open: true,
-  },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  };
 };
