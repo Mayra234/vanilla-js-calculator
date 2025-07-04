91e797c342579a0abac48169f627543ecdb2b237
Alejandra Diavanera <alejandra.diavanera@confeorg.com>
2025-07-04 16:16:54 -0500

    Funcionalidad solo impresion de botones

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './index.css';
 
-export const Button = ({ children = '', className = '' }) => {
-  return <button className={'button ' + className}>{children}</button>;
+export const Button = ({ children, className, type, onClick }) => {
+  return (
+    <button type={type} onClick={onClick} className={'button ' + className}>
+      {children}
+    </button>
+  );
 };
--- src/pages/Calculator/index.jsx
+++ src/pages/Calculator/index.jsx
@@ -1,44 +1,147 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import { Separator } from '../../components/Separator';
-import { CalculatorScreen } from '../../components/CalculatorScreen';
 import { Button } from '../../components/Button';
+import { Box } from '@mui/material';
 
 export const Calculator = () => {
+  const [resultado, setResultado] = useState('');
+
+  const handleClick = (valor) => {
+    setResultado((prev) => prev + valor);
+  };
   return (
-    <div className="calculator">
+    <div id="calculator">
       <div className="screen">
-        <CalculatorScreen>
+        <Box id="result">
           <span className="number">2</span>
           <span className="operation-symbol">x</span>
           <span className="number">20</span>
           <span className="operation-symbol">x</span>
           <span className="number">2</span>
-        </CalculatorScreen>
+          {resultado}
+        </Box>
         <div className="operation-result">300</div>
       </div>
       <Separator />
       <div className="keyboard">
-        <Button className="logical-operator">c</Button>
-        <Button className="logical-operator">+/-</Button>
-        <Button className="logical-operator">%</Button>
-        <Button className="arithmetic-operators">/</Button>
-        <Button className="logical-operator">7</Button>
-        <Button className="logical-operator">8</Button>
-        <Button className="logical-operator">9</Button>
-        <Button className="arithmetic-operators">x</Button>
-        <Button className="numeric-keyboard">4</Button>
-        <Button className="numeric-keyboard">5</Button>
-        <Button className="numeric-keyboard">6</Button>
-        <Button className="arithmetic-operators">-</Button>
-        <Button className="numeric-keyboard">1</Button>
-        <Button className="numeric-keyboard">2</Button>
-        <Button className="numeric-keyboard">3</Button>
-        <Button className="arithmetic-operators">+</Button>
-        <Button className="numeric-keyboard">00</Button>
-        <Button className="numeric-keyboard">0</Button>
-        <Button className="numeric-keyboard">.</Button>
-        <Button className="equal-key">=</Button>
+        <Button
+          onClick={() => handleClick('')}
+          className="button logical-operator"
+        >
+          AC
+        </Button>
+        <Button
+          onClick={() => handleClick('%')}
+          className="button logical-operator"
+        >
+          %
+        </Button>
+        <Button
+          onClick={() => handleClick('')}
+          className="button logical-operator"
+        >
+          ---
+        </Button>
+        <Button
+          onClick={() => handleClick('/')}
+          className="button arithmetic-operators"
+        >
+          /
+        </Button>
+        <Button
+          onClick={() => handleClick('7')}
+          className="button logical-operator"
+        >
+          7
+        </Button>
+        <Button
+          onClick={() => handleClick('8')}
+          className="button logical-operator"
+        >
+          8
+        </Button>
+        <Button
+          onClick={() => handleClick('9')}
+          className="button logical-operator"
+        >
+          9
+        </Button>
+        <Button
+          onClick={() => handleClick('x')}
+          className="button arithmetic-operators"
+        >
+          x
+        </Button>
+        <Button
+          onClick={() => handleClick('4')}
+          className="button numeric-keyboard"
+        >
+          4
+        </Button>
+        <Button
+          onClick={() => handleClick('5')}
+          className="button numeric-keyboard"
+        >
+          5
+        </Button>
+        <Button
+          onClick={() => handleClick('6')}
+          className="button numeric-keyboard"
+        >
+          6
+        </Button>
+        <Button
+          onClick={() => handleClick('-')}
+          className="button arithmetic-operators"
+        >
+          -
+        </Button>
+        <Button
+          onClick={() => handleClick('1')}
+          className="button numeric-keyboard"
+        >
+          1
+        </Button>
+        <Button
+          onClick={() => handleClick('2')}
+          className="button numeric-keyboard"
+        >
+          2
+        </Button>
+        <Button
+          onClick={() => handleClick('3')}
+          className="button numeric-keyboard"
+        >
+          3
+        </Button>
+        <Button
+          onClick={() => handleClick('+')}
+          className="button arithmetic-operators"
+        >
+          +
+        </Button>
+        <Button
+          onClick={() => handleClick('00')}
+          className="button numeric-keyboard"
+        >
+          00
+        </Button>
+        <Button
+          onClick={() => handleClick('0')}
+          className="button numeric-keyboard"
+        >
+          0
+        </Button>
+        <Button
+          onClick={() => handleClick('.')}
+          className="button numeric-keyboard"
+        >
+          .
+        </Button>
+        <Button onClick={() => handleClick('')} className="button equal-key">
+          =
+        </Button>
       </div>
     </div>
   );
